1e38060406156c41aa39c7e085c113513195c5b8
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-04-26 11:12:01 +0500

    User Dto

---
+++ src/UserApp.Service/DTOs/Users/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using UserApp.Domain.Enums;
+
+namespace UserApp.Service.DTOs.Users;
+
+public class UserViewModel
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public UserRole Role { get; set; }
+    public long? AssetId { get; set; }
+}
---
+++ src/UserApp.Service/Services/Users/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using UserApp.Service.Configurations;
+using UserApp.Service.DTOs.Users;
+
+namespace UserApp.Service.Services.Users;
+
+public interface IUserService
+{
+    ValueTask<UserViewModel> CreateAsync(UserCreateModel model);
+    ValueTask<UserViewModel> UpdateAsync(long id, UserUpdateModel model);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<UserViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<UserViewModel>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+}
