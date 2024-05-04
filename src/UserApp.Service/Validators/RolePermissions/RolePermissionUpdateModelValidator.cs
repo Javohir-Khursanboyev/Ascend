a7cc95596ff61de6cd359c3caf46c39728197919
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-04 22:29:36 +0500

    Added RolePermission Create and Update Model Validator

---
+++ src/UserApp.Service/Validators/RolePermissions/RolePermissionUpdateModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using UserApp.Service.DTOs.RolePermissions;
+
+namespace UserApp.Service.Validators.RolePermissions;
+
+public class RolePermissionUpdateModelValidator : AbstractValidator<RolePermissionUpdateModel>
+{
+    public RolePermissionUpdateModelValidator()
+    {
+        RuleFor(asset => asset.RoleId)
+            .NotNull()
+            .WithMessage(asset => $"{nameof(asset.RoleId)} is not specified");
+
+        RuleFor(asset => asset.PermissionId)
+           .NotNull()
+           .WithMessage(asset => $"{nameof(asset.PermissionId)} is not specified");
+    }
+}
